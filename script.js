4fa9a1c1c5138e18c2a69f70271fecff200da075
vashivan <123917348+vashivan@users.noreply.github.com>
2023-11-23 14:57:36 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -8,23 +8,49 @@ document.addEventListener('DOMContentLoaded', function() {
   const cupsPerDayElement = document.getElementById('cupsPerDay');
   const startReminderButton = document.getElementById('startReminder');
   const drinkenCupsButton = document.getElementById('drinkenCups');
+  const countdownTimerElement = document.getElementById('countdownTimer');
+  const resetButtonElement = document.getElementById('reset');
 
   dailyIntakeElement.textContent = `${dailyIntakeWater} мл`;
   cupsPerDayElement.textContent = `${cupsPerDay} склянок`;
   drinkenCupsButton.textContent = `Випито: ${drinkenCups}`;
+  resetButtonElement.textContent = 'Обнуляймося';
 
   let reminderInterval;
+  let countdownTimer;
 
   function remindTimer() {
     alert('Випийте склянку води!');
+    startCountdown();
+  }
+
+  function startCountdown() {
+    let secondsLeft = 60 * 60;
+    countdownTimer = setInterval(function() {
+      const min = Math.floor(secondsLeft / 60);
+      const sec = secondsLeft % 60;
+
+      countdownTimerElement.textContent = `Наступне нагадуваня через ${min} хв та ${sec} сек`;
+
+      if (secondsLeft === 0) {
+        clearInterval(countdownTimer);
+        remindTimer();
+      }
+
+      secondsLeft--;
+    }, 1000);
   }
 
   startReminderButton.addEventListener('click', function() {
+    remindTimer();
     reminderInterval = setInterval(remindTimer, 60 * 60 * 1000);
-    alert('Нагадвання розпочато!');
+    alert('Нагадування розпочато!');
   });
 
   drinkenCupsButton.addEventListener('click', function() {
+    clearInterval(reminderInterval);
+    clearInterval(countdownTimer);
+    startCountdown();
     drinkenCups++;
     dailyIntakeWater -= cup;
     cupsPerDay--;
@@ -37,14 +63,36 @@ document.addEventListener('DOMContentLoaded', function() {
       dailyIntakeWater = 2000;
       cupsPerDay = 8;
       drinkenCups = 0;
+
+      clearInterval(reminderInterval);
+      clearInterval(countdownTimer);
   
       dailyIntakeElement.textContent = `${dailyIntakeWater} мл`;
       cupsPerDayElement.textContent = `${cupsPerDay} склянок`;
       drinkenCupsButton.textContent = `Випито: ${drinkenCups}`;
+      countdownTimerElement.textContent = '';
   
       clearInterval(remindTimer);
+      clearInterval(countdownTimer);
   
       alert('Вітаю, ви упісяєтесь незабаром!:)');
     }
   });
-});
+
+  resetButtonElement.addEventListener('click', function() {
+    dailyIntakeWater = 2000;
+    cupsPerDay = 8;
+    drinkenCups = 0;
+
+    clearInterval(reminderInterval);
+    clearInterval(countdownTimer);
+    clearInterval(remindTimer);
+
+    dailyIntakeElement.textContent = `${dailyIntakeWater} мл`;
+    cupsPerDayElement.textContent = `${cupsPerDay} склянок`;
+    drinkenCupsButton.textContent = `Випито: ${drinkenCups}`;
+    countdownTimerElement.textContent = '';
+
+    alert('Ок, зупиняємо лічильник. Тож ви засохнете і помрете');
+  })
+})
